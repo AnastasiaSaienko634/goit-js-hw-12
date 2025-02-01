91183f8162c76a168ac152f981bfc409685e6674
nasti <anastasiasaienko634@gmail.com>
2025-02-01 15:48:26 +0100

    update all

--- src/js/render-funktions.js
+++ src/js/render-funktions.js
@@ -7,10 +7,10 @@ export function galleryFn (ImgInfo) {
     <img class="photo" src='${ImgInfo.webformatURL}' alt='${ImgInfo.tags}' width='360' height='160'>
     </a>
     <div class="text">
-    <p class="tags">Likes<span class="positions">${ImgInfo.likes}</span></p>
-    <p class="views">Views<span class="positions">${ImgInfo.views}</span></p>
-    <p class="comments">Comments<span class="positions">${ImgInfo.comments}</span></p>
-    <p class="downloads">Downloads<span class="positions">${ImgInfo.downloads}</span></p>
+    <p class="tags"><svg class="icon icon-heart" width="20" heigth="20"><use href="../icon/symbol-defs.svg#icon-heart"></use></svg><span class="text-card">Likes</span><span class="positions">${ImgInfo.likes}</span></p>
+    <p class="views"><svg class="icon icon-visibility" width="20" heigth="20"><use href="../icon/symbol-defs.svg#icon-visibility"></use></svg><span class="text-card">Views</span><span class="positions">${ImgInfo.views}</span></p>
+    <p class="comments"><svg class="icon icon-comment" width="20" heigth="20"><use href="../icon/symbol-defs.svg#icon-comment"></use></svg><span class="text-card">Comments</span><span class="positions">${ImgInfo.comments}</span></p>
+    <p class="downloads"><svg class="icon icon-cloud-download" width="20" heigth="20"><use href="../icon/symbol-defs.svg#icon-cloud-download"></use></svg><span class="text-card">Downloads</span><span class="positions">${ImgInfo.downloads}</span></p>
     </div>
     </li>`
 };
